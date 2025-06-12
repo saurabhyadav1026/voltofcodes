04ce4a2a5f5917d3c8a180f526c97bc262542a22
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-06-12 11:27:45 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,9 +1,9 @@
-def convert_temperature(temp, type):
-    if type == 'F':
+def convert_temperature(temp, unit):
+    if unit == 'F':
         # Convert Fahrenheit to Celsius
         celsius = (temp - 32) * 5/9
         return round(celsius, 2)
-    elif type == 'C':
+    elif unit == 'C':
         # Convert Celsius to Fahrenheit
         fahrenheit = (temp * 9/5) + 32
         return round(fahrenheit, 2)
